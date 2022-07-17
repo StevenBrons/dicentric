70166dedc00c1ced3455771ecc9adcdd9e37db4f
Sieeena <siena.van.schaick@gmail.com>
2022-07-17 14:44:16 +0200

    .

---
+++ src/game_logic/items/equipment/averageOnly.ts
@@ -0,0 +1,15 @@
+import { DICE } from "../dice";
+import Equipment from "../equipment";
+
+class AverageOnly extends Equipment {
+    description: string = "Only roll the middle two numbers of the die.";        
+
+    rollWithEq(d: DICE): number {
+        if(Math.random() > 0.5) {
+            return Math.floor(d/2);
+        }
+        return Math.ceil(d/2);
+    }
+}
+
+export default AverageOnly;
--- src/game_logic/items/equipment/onlyEven.ts
+++ src/game_logic/items/equipment/onlyEven.ts
@@ -2,10 +2,13 @@ import { DICE } from "../dice";
 import Equipment from "../equipment";
 
 class OnlyEven extends Equipment {
-    description: string = "Prevents the die from rolling 1";        
+    description: string = "Only roll even numbers.";        
 
     rollWithEq(d: DICE): number {
         let roll = Math.floor(Math.random() * (d - 1) + 1);
+        if(roll%2 === 1) {
+            roll = roll+1;
+        }
         return roll;
     }
 }
