d7a897612bd0b4d651cfefa89558b3a34c609c96
Sieeena <siena.van.schaick@gmail.com>
2022-07-17 15:05:01 +0200

    .

--- src/levels/level1.ts
+++ src/levels/level1.ts
@@ -30,8 +30,8 @@ let battle2 = new BattleEvent([{text : "attack the amount rolled", slots: 1, eff
 let bossbattle = new BattleEvent([{text : "attack the amount rolled", slots: 2, effect: BATTLE_ACTION.attack}, {text : "defend, heal the amount rolled more than the enemies' roll", slots: 3, effect: BATTLE_ACTION.defend}], boss);
 
 //construct dialogue events
-let tutorialDialogue = new DialogueEvent([{text : "Search thoroughly, roll higher than 4 to succeed", slots : 2, effect : {succes: (n: number)=>{return n>4?{succes : true, text : "You found some dice!"}:{succes : false, text : "You didn't find anything."}}, rewards: [new Dice(DICE.d6), new Dice(DICE.d4), new Dice(DICE.d4)]}}
-	, {text : "Search for a bit, roll higher than 2 to succed", slots : 1, effect : {succes: (n: number)=>{return n>2?{succes : true, text : "You found a die!"}:{succes : false, text : "You didn't find anything."}}, rewards: [new Dice(DICE.d6)]}}]
+let tutorialDialogue = new DialogueEvent([{text : "Search thoroughly, roll higher than 4 to succeed", slots : 2, effect : {succes: (n: number)=>{return n>4?{succes : true, text : "You found some dice! \n d4 was added to your inventory. \n d4 was added to your inventory. \n d6 was added to you inventory."}:{succes : false, text : "You didn't find anything."}}, rewards: [new Dice(DICE.d6), new Dice(DICE.d4), new Dice(DICE.d4)]}}
+	, {text : "Search for a bit, roll higher than 2 to succed", slots : 1, effect : {succes: (n: number)=>{return n>2?{succes : true, text : "You found a die! \n d6 was added to your inventory."}:{succes : false, text : "You didn't find anything."}}, rewards: [new Dice(DICE.d6)]}}]
 	, "Welcome to Dicentric! You will move accross islands. Dice are used for every action. Your rolls determine the outcome. There seems to be something in the grass. Search the grass? You can choose not to by closing the window");
 
 let junctionDialogue = new DialogueEvent([]
@@ -42,13 +42,13 @@ let equipmentDialogue = new DialogueEvent([{text : "Pick up the object. Roll any
 
 
 //construct nodes
-let node7 = new MapNode([],439,80,5,bossbattle,true,"");
-let node6 = new MapNode([node7],352,80,4,equipmentDialogue,true,"");
-let node5 = new MapNode([node6],233,128,3,battle1,true,"");
-let node4 = new MapNode([node6],288,32,3,battle2,true,"");
-let node3 = new MapNode([node4, node5],224,80,2,junctionDialogue,true,"");
-let node2 = new MapNode([node3],144,80,1,shop,true,"");
-let node1 = new MapNode([node2],64,80,0,tutorialDialogue,true,"");
+let node7 = new MapNode([],439,80,5,bossbattle,true,"island_boss");
+let node6 = new MapNode([node7],352,80,4,equipmentDialogue,true,"island_event");
+let node5 = new MapNode([node6],233,128,3,battle1,true,"island_enemy_bird");
+let node4 = new MapNode([node6],288,32,3,battle2,true,"island_enemy_cube");
+let node3 = new MapNode([node4, node5],224,80,2,junctionDialogue,true,"island_event");
+let node2 = new MapNode([node3],144,80,1,shop,true,"island_shop");
+let node1 = new MapNode([node2],64,80,0,tutorialDialogue,true,"island_event");
 
 //construct map
 let map = new MapState([node1, node2, node3, node4, node5, node6, node7], [], "./res/level_1_map.png", {width : 640, height : 180}); //idk of path naar image klopt
