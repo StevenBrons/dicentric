83d564f1e19b5ce373123cfee83c2d9a024d7544
StevenBrons <smp15.programming@gmail.com>
2022-07-17 10:14:04 +0200

    Start shop

--- src/ui/screens/Shop.tsx
+++ src/ui/screens/Shop.tsx
@@ -1,15 +1,32 @@
 import { FC } from "react";
+import ShopEvent from "../../game_logic/events/shopEvent";
+import ActionBar from "../ActionBar";
+import ItemSlot from "../ItemSlot";
 import Screen from "./Screen";
 import "./Shop.css";
 
-const Shop : FC = () => {
+interface Props {
+	shopEvent: ShopEvent
+}
+
+const Shop : FC<Props> = ({ shopEvent}) => {
+	const stock = shopEvent.stock.map((item, i) => <div>
+		<ItemSlot item={item.sold ? null : item.item} key={i} placeHolder={item.item.image}/>
+		<span>
+			{item.price}
+		</span>
+		</div>)
 	return <Screen name="Shop">
 		<h1>
 			Shop
 		</h1>
-		<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas efficitur ullamcorper tincidunt. Morbi vel sem sed quam iaculis pulvinar sit amet id velit.
-		</p>
+		<div>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas efficitur ullamcorper tincidunt.
+		</div>
+		<div className="Stock">
+			{stock}
+		</div>
+		{shopEvent.actions.map(a => <ActionBar action={a}/>)}
 	</Screen>
 }
 
